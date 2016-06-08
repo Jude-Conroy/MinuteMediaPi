87ea534d85ea4725c58ae8f9f7d5dcb2ee912925
Jude-Conroy <judejconroy@gmail.com>
2016-06-08 10:45:18 +0100

    added getters and setters

--- src/main/java/com/minutemedia/pi/PiData.java
+++ src/main/java/com/minutemedia/pi/PiData.java
@@ -3,6 +3,42 @@ package com.minutemedia.pi;
 public class PiData {
 
 	
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getMac_short() {
+		return mac_short;
+	}
+	public void setMac_short(String mac_short) {
+		this.mac_short = mac_short;
+	}
+	public String getMac() {
+		return mac;
+	}
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+	public String getVenueid() {
+		return venueid;
+	}
+	public void setVenueid(String venueid) {
+		this.venueid = venueid;
+	}
+	public String getSamples() {
+		return samples;
+	}
+	public void setSamples(String samples) {
+		this.samples = samples;
+	}
+	public String getCaptime() {
+		return captime;
+	}
+	public void setCaptime(String captime) {
+		this.captime = captime;
+	}
 	public String distance;
 	public String mac_short;
 	public String mac;
@@ -11,3 +47,4 @@ public class PiData {
 	public String captime;
 
 }
+
